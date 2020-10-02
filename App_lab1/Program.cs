3b54be761411e6861d8a6885d974560dab0a0e2a
Nataliia Byshovets <natashabysh11@gmail.com>
2020-10-02 16:21:40 +0300

    Add project files.

---
+++ App_lab1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Collection_lab1;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace lab1_Byshovets
+{
+    class Program
+    {
+        static NodeStack<string> stack;
+        static string message;
+        static void Main(string[] args)
+        {
+            stack = new NodeStack<string>();
+
+            stack.CollectionChanged += Stack_CollectionChanged;
+
+            int key = 1, answer;
+            string value = "", peek, del;
+            while (key != 0)
+            {
+                Console.Clear();
+                Console.Write(
+                    "           Menu:\n" +
+                    "1: Add value to stack.\n" +
+                    "2: Show all values from stack.\n" +
+                    "3: Show top value of stack.\n" +
+                    "4: Delete value from stack.\n" +
+                    "Enter key: ");
+
+                key = Convert.ToInt32(Console.ReadLine());
+
+                switch (key)
+                {
+                    //добавляем элементы в стек
+                    case 1:
+                        Console.Write("\nHow many values do you want to insert?: ");
+                        answer = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < answer; i++)
+                        {
+                            Console.Write("Add value: ");
+                            value = Console.ReadLine();
+                            stack.Push(value);
+
+                            if (message != null)
+                            {
+                                Console.WriteLine($"\n{message}\n");
+                            }
+                        }
+                        break;
+                    //выводим вместимое стека
+                    case 2:
+                        try
+                        {
+                            stack.ReturnEmpty();
+                            Console.WriteLine("\nStack values:\n");
+                            foreach (var elem in stack)
+                            {
+                                Console.WriteLine(elem);
+                            }
+                            Console.WriteLine();
+                        }
+                        catch (InvalidOperationException err)
+                        {
+                            Console.WriteLine(err.Message);
+                        }
+                        break;
+                    case 3:
+                        try
+                        {
+                            stack.ReturnEmpty();
+                            peek = stack.Peek();
+                            Console.WriteLine($"\nTop value in stack: {peek}\n");
+                        }
+                        catch (InvalidOperationException err)
+                        {
+                            Console.WriteLine(err.Message);
+                        }
+                        break;
+                    case 4:
+                        try
+                        {
+                            stack.ReturnEmpty();
+                            del = stack.Pop();
+                            Console.WriteLine($"\nDeleted value is: {del}");
+
+                            if (message != null)
+                            {
+                                Console.WriteLine($"\n{message}\n");
+                            }
+                        }
+                        catch (InvalidOperationException err)
+                        {
+                            Console.WriteLine(err.Message);
+                        }
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("\nWrong input! Try again.\n");
+                        break;
+                }
+                Console.Write("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        private static void Stack_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            message = "What has changed in the stack? :)\n\n";
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    message += $"Action 'Push' - add items to stack:\n" +
+                        $"Your new item is '{e.NewItems[0]}'\n";
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    message += $"Action 'Pop' - delete item from stack:\n" +
+                        $"Your deleted item is '{e.OldItems[0]}'\n";
+                    break;
+            }
+            message += $"Amount of items in stack: {((NodeStack<string>)sender).Count}";
+        }
+    }
+}
---
+++ Collection_lab1/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace Collection_lab1
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+
+    public class NodeStack<T> : IEnumerable<T>, ICollection, INotifyCollectionChanged
+    {
+        Node<T> head;
+        int count;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private bool IsEmpty
+        {
+            get
+            {
+                return count == 0;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public object SyncRoot => this; //передаваемый обьект стека для начала синхронизации
+        public bool IsSynchronized => false; //мультипоточность запрещена! (нельзя использовать синхронизацию)
+
+        public void Push(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            node.Next = head;
+            head = node;
+            count++;
+
+            CollectionChanged?.Invoke(this,
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Add, node.Data)
+                );
+        }
+
+        public T Pop()
+        {
+            ReturnEmpty();
+            Node<T> temp = head;
+            head = head.Next;
+            count--;
+
+            CollectionChanged?.Invoke(this,
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Remove, temp.Data)
+                );
+
+            return temp.Data;
+        }
+
+        public T Peek()
+        {
+            ReturnEmpty();
+            return head.Data;
+        }
+
+        public void ReturnEmpty()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("\nStack is empty!");
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            for (Node<T> current = head; current != null; current = current.Next)
+            {
+                yield return current.Data;
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            int item = 0;
+
+            for (Node<T> node = head; node != null; node = node.Next, item++)
+            {
+                array.SetValue(node.Data, item);
+            }
+        }
+    }
+}
